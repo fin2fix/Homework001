b0e7e1963a1233f671ebe19f8724b997112a8b48
KMikhail <fin2fix@gmail.com>
2023-01-16 15:39:24 +0000

    Formatted

--- Zadanie03/Program.cs
+++ Zadanie03/Program.cs
@@ -2,7 +2,7 @@
 string numberStr1 = Console.ReadLine()!;
 int a = Convert.ToInt32(numberStr1);
 
-if (a%2 == 0)
+if (a % 2 == 0)
 { Console.WriteLine($"Введенное число {a} - четное"); }
 else
 { Console.WriteLine($"Введенное число {a} - нечетное"); }
--- Zadanie04/Program.cs
+++ Zadanie04/Program.cs
@@ -3,6 +3,6 @@ string numberStr1 = Console.ReadLine()!;
 int a = Convert.ToInt32(numberStr1);
 Console.WriteLine("------------");
 
-for (int i = 2; i <= a; i+=2)
-{  Console.Write($" {i} "); }
+for (int i = 2; i <= a; i += 2)
+{ Console.Write($" {i} "); }
 
