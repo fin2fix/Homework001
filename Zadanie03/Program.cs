45aaf161a66a26718123f3c9e0cf1d2117ac5bd0
KMikhail <fin2fix@gmail.com>
2023-01-16 14:43:33 +0000

    Add Zadanie03

---
+++ Zadanie03/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+string numberStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numberStr1);
+
+if (a%2 == 0)
+{ Console.WriteLine($"Введенное число {a} - четное"); }
+else
+{ Console.WriteLine($"Введенное число {a} - нечетное"); }
