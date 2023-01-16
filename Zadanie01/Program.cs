21c542cb1df9fb11d43433d7b3dc6267b83a219a
KMikhail <fin2fix@gmail.com>
2023-01-16 14:23:36 +0000

    Add Zadanie01

---
+++ Zadanie01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+string numberStr1 = Console.ReadLine();
+double a = Convert.ToDouble(numberStr1);
+
+Console.WriteLine("Введите второе число");
+string numberStr2 = Console.ReadLine();
+double b = Convert.ToDouble(numberStr2);
+
+if (a > b)
+{
+  Console.WriteLine($"Первое число {a} больше второго числа {b}");
+}
+else
+{
+  Console.WriteLine($"Второе число {b} больше первого числа {a}");
+}
