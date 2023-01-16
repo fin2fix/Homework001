e7a01666ee59fd1cfd45feddc5cdee57a385fe3b
KMikhail <fin2fix@gmail.com>
2023-01-16 14:54:13 +0000

    Add Zadanie04

---
+++ Zadanie04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+string numberStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numberStr1);
+Console.WriteLine("------------");
+
+for (int i = 2; i <= a; i+=2)
+{  Console.Write($" {i} "); }
+
